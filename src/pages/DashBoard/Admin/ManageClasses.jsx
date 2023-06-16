f4ab45cbbd140bca0329fff94440db0d1db62d19
git4novel <novelsylhet@gmail.com>
2023-06-17 02:27:09 +0600

    mangage classes

--- src/pages/DashBoard/Admin/ManageClasses.jsx
+++ src/pages/DashBoard/Admin/ManageClasses.jsx
@@ -1,10 +1,187 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { useState } from "react";
 
 const ManageClasses = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [axiosSecure] = useAxiosSecure();
+  const { data: classes = [], refetch } = useQuery(["classes"], async () => {
+    const res = await axiosSecure.get("/allclasses");
+    console.log(res.data);
+    return res.data;
+  });
+
+  const handleApprove = (cls) => {
+    fetch(`https://summercamp-ten.vercel.app/classes/approve/${cls._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${cls.Name} is approved`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const handleDeny = (cls) => {
+    fetch(`https://summercamp-ten.vercel.app/classes/deny/${cls._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${cls.Name} is approved`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  }; 
+  
+  const [feedbackMessage, setFeedbackMessage] = useState(""); // State for feedback message
+  const [selectedClass, setSelectedClass] = useState(null); // State for selected class
+
+  const handleFeedback = () => {
+    if (selectedClass) {
+      fetch(`https://summercamp-ten.vercel.app/classes/feedback/${selectedClass._id}`, {
+        method: "PATCH",
+        body: JSON.stringify({ message: feedbackMessage }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.modifiedCount) {
+            refetch();
+            Swal.fire({
+              position: "center",
+              icon: "question",
+              title: `${selectedClass.Name} is given feedback`,
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+    }
+  };
+
+  const handleModalSubmit = (event) => {
+    event.preventDefault();
+    // Set the feedback message
+    const feedbackTextArea = document.getElementById("feedbackTextArea");
+    setFeedbackMessage(feedbackTextArea.value);
+    // Close the modal
+    const myModal = document.getElementById("my_modal_1");
+    myModal.close();
+    // Call handleFeedback
+    handleFeedback();
+  };
+
+
+  return (
+    <div className="p-3 m-3">
+      <h4 className="text-xl font-semibold text-center">Manage Users</h4>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Class</th>
+              <th>Instructor</th>
+              <th>Available Seats</th>
+              <th>Price</th>
+              <th>status</th>
+              <th>Buttons</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {classes.map((cls) => (
+              <tr key={cls._id}>
+                {/* classname and Img */}
+                <td>
+                  <p>{cls?.Name}</p>
+                  <img className="w-12 rounded-md" src={cls.ClassImg} alt="" />
+                </td>
+                {/* instructor name and email */}
+                <td>
+                  <p>{cls.InstructorName}</p>
+                  <p>{cls.email}</p>
+                </td>
+                <td>{cls?.availableseat}</td>
+                <td>{cls?.price}</td>
+                <td>{cls?.pendingStatus}</td>
+                <td>
+                  {cls.pendingStatus === ("approved" || "denied") ? (
+                    <div className="flex flex-col gap-1">
+                      <button className="btn btn-disabled btn-sm text-sm">
+                        Approve
+                      </button>
+                      <button className="btn btn-disabled  btn-sm text-sm">
+                        Deny
+                      </button>
+                      <button className="btn btn-disabled  btn-sm text-sm">
+                        Feedback
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="flex flex-col gap-1">
+                      <button
+                        onClick={() => handleApprove(cls)}
+                        className="btn btn-success  btn-sm text-sm"
+                      >
+                        Approve
+                      </button>
+                      <button
+                        onClick={() => handleDeny(cls)}
+                        className="btn btn-primary  btn-sm text-sm"
+                      >
+                        Deny
+                      </button>
+                      <button
+                        onClick={() => {
+                            setSelectedClass(cls);
+                            const myModal = document.getElementById("my_modal_1");
+                            myModal.showModal();
+                          }}
+                        className="btn btn-secondary  btn-sm text-sm"
+                      >
+                        Feedback
+                      </button>
+                    </div>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+       {/* Feedback modal */}
+       <dialog id="my_modal_1" className="modal">
+        <form onSubmit={handleModalSubmit} className="modal-box">
+          <h3 className="font-bold text-lg">Feedback</h3>
+          <p className="py-4">Press ESC key or click the button below to close</p>
+          <textarea id="feedbackTextArea" name="feedback" className="bg-gray-300"></textarea>
+          <div className="modal-action">
+            <button type="submit" className="btn">
+              Send Feedback
+            </button>
+          </div>
+        </form>
+      </dialog>
+    </div>
+  );
 };
 
 export default ManageClasses;
--- src/pages/DashBoard/Admin/ManageUser.jsx
+++ src/pages/DashBoard/Admin/ManageUser.jsx
@@ -1,4 +1,53 @@
+
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
+
 const ManageUser = () => {
+  const [axiosSecure] = useAxiosSecure();
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await axiosSecure.get("/users");
+    console.log(res.data);
+    return res.data;
+  });
+
+  const handleMakeAdmin = (user) => {
+    fetch(`https://summercamp-ten.vercel.app/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${user.name} is an Admin Now`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const handleMakeInstructor = (user) => {
+    fetch(`https://summercamp-ten.vercel.app/users/instructor/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${user.name} is an Instructor`,
+            showConfirmButton: false,
+            timer: 1000,
+          });
+        }
+      });
+  };
+
   return (
     <div className="p-3 m-3">
       <h4 className="text-xl font-semibold text-center">Manage Users</h4>
@@ -7,53 +56,48 @@ const ManageUser = () => {
           {/* head */}
           <thead>
             <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
               <th>User Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Mail</th>
+              <th>role</th>
+              <th>Make Instructor</th>
+              <th>Make admin</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-2@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.role ? user.role : "user"}</td>
+                <td>
+                  {user.role === "instructor" ? (
+                    <button className="btn btn-disabled">
+                      Make instructor
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeInstructor(user)}
+                      className="btn btn-primary"
+                    >
+                      Make instructor
+                    </button>
+                  )}
+                </td>
+                <td>
+                  {user.role === "admin" ? (
+                    <button className="btn btn-disabled">Make Admin</button>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-secondary"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
